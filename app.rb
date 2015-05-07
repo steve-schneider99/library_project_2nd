7c70ae91d7de445776b4de1394a3305cafd2c23a
Jeanette Fairless & Steve Schneider <student@epicodus.com>
2015-05-07 15:56:22 -0700

    add patron_books code and patron_search

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require('pry')
 DB = PG.connect({:dbname => "library_project_test"})
 
 get('/') do
+  @patrons = Patron.all_patrons()
   erb(:index)
 end
 
@@ -34,8 +35,8 @@ end
 patch('/book/:id') do
   title = params.fetch("title")
   author = params.fetch("author")
-  if title == "" {title = nil}
-  if author == "" {author = nil}
+  # if title == "" {title = nil}
+  # if author == "" {author = nil}
   @book = Book.find(params.fetch("id").to_i())
   @book.update({:title => title, :author => author})
   erb(:librarian_book)
@@ -46,4 +47,20 @@ delete('/book/:id') do
   @book.delete()
   @books = Book.all_books()
   erb(:librarian_books)
+end
+
+get('/patron/:id') do
+  @patron = Patron.find(params.fetch('id').to_i())
+  erb(:patron_books)
+end
+
+post('/new_patron') do
+  name = params.fetch("name")
+  new_patron = Patron.new({:name => name, :id => nil})
+  new_patron.save()
+  @patrons = Patron.all_patrons()
+  erb(:index)
+end
+
+get('/author_results') do
 end
